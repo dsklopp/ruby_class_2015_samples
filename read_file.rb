ade16b1fe42cb6fec4972d693c0967d10bda4625
git <dsklopp@gmail.com>
2014-12-28 08:36:13 -0800

    Added comments

--- read_file.rb
+++ read_file.rb
@@ -1,3 +1,7 @@
+# Copyright:   GPL
+# Author:      Daniel Klopp (dsklopp@gmail.com)
+# Created:     December 28, 2014 (12/28/14)
+# Description: Demonstrate one way to open a file in Ruby and print it out
 
 host_file = File.open("/etc/hosts")
 
