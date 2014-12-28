f5fccc146de87142234ee401ecb5d12f5a379a87
git <dsklopp@gmail.com>
2014-12-28 08:47:33 -0800

    Changed method calls to always have parenthesis

--- read_file.rb
+++ read_file.rb
@@ -2,12 +2,14 @@
 # Author:      Daniel Klopp (dsklopp@gmail.com)
 # Created:     December 28, 2014 (12/28/14)
 # Description: Demonstrate one way to open a file in Ruby and print it out
+#              we start it with using parenthesis, then demonstrate they are
+#              not needed.
 
 host_file = File.open("/etc/hosts")
 
-host_file.readlines.each do |line|
+host_file.readlines().each() do |line|
    puts line
 end
 
-host_file.close
+host_file.close()
 
