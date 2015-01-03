24224500b790e649b8d80727f107abd09b6d51a4
git <dsklopp@gmail.com>
2015-01-03 15:38:28 -0800

    First slide of arrays

---
+++ arrays.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# Copyright:   GPL
+# Author:      Daniel Klopp (dsklopp@gmail.com)
+# Created:     January 03, 2015 (01/03/15)
+# Description: Demonstrate arrays
+
+
+# For class demonstration, start irb likeso
+# irb --noecho --simple-prompt
+# then copy and paste until the next comment
+basic_array = [ 'a', 'b', 'c', 'd']
+print basic_array, "\n"
+print basic_array[1], "\n"
+basic_array << 'e'
+print basic_array, "\n"
+print basic_array.reverse(), "\n"
+
+# End string splicing demonstration
+
+# For class demonstration, start irb likeso
+# irb --noecho --simple-prompt
+# then copy and paste until the next comment
+phrase="You flatter me.  I would have reserved a seat specifically for you."
+# End string splicing demonstration
+
