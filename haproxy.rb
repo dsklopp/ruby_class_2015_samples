ac9ac9d87509597ffb33448caed93dcb08735289
Dan Klopp <dsklopp@gmail.com>
2015-01-02 18:28:13 -0800

    Adding author, copyright and description

--- haproxy.rb
+++ haproxy.rb
@@ -1,6 +1,12 @@
+# Author: Daniel Klopp
+# Copyright: GPL2
+# Description: Sample ruby code that opens file and prints it out
+#              Demonstrates File Output, file iteration, and each loops
 
+ 
 a_file=File.open("/etc/haproxy/haproxy.cfg")
 
+#
 a_file.readlines.each do |line|
   puts line
 end
